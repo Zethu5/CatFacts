c156964d9ee6926dbc64711515788ad69bb0dad1
Zethus <ravet.zvika93@gmail.com>
2023-04-20 21:40:21 +0300

    test

--- terraform/create_catfacts_kubernetes_deployment/create_catfacts_kubernetes_deployment.tf
+++ terraform/create_catfacts_kubernetes_deployment/create_catfacts_kubernetes_deployment.tf
@@ -18,6 +18,8 @@ variable "GOOGLE_CREDENTIALS" {
 
 provider "kubernetes" {
   host             = google_container_cluster.cluster.endpoint
+  token            = data.google_client_config.current.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
 }
 
 provider "google" {
