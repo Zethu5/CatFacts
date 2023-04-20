699aca8855d408d360bc9b8e1feaf9479d8e7471
Zethus <ravet.zvika93@gmail.com>
2023-04-20 16:07:16 +0300

    allow everyone in the app

--- catfacts/settings.py
+++ catfacts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0q88!8-bely5@^_*f4$&5ushn70_0tpmf0)4j0)u=i#sr&hurf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
