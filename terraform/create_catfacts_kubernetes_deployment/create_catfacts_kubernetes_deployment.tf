dc48352da4fc7a52b0f86e0f042c16495a75b543
Zethus <ravet.zvika93@gmail.com>
2023-04-20 21:28:02 +0300

    create catfacts deployment

---
+++ terraform/create_catfacts_kubernetes_deployment/create_catfacts_kubernetes_deployment.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 3.0"
+    }
+    kubernetes = {
+        source  = "hashicorp/kubernetes"
+        version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  load_config_file = false
+  host             = "catfacts"
+}
+
+provider "google" {
+  project = "catfacts-384312"
+  region  = "europe-west1"
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+resource "kubernetes_deployment" "catfacts" {
+  metadata {
+    name = "catfacts"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "catfacts"
+      }
+    }
+
+    replicas = 3
+
+    template {
+      metadata {
+        labels = {
+          app = "catfacts"
+        }
+      }
+
+      spec {
+        container {
+          name  = "catfacts"
+          image = "zethu5/catfacts"
+
+          port {
+            container_port = 7777
+          }
+        }
+      }
+    }
+  }
+}
