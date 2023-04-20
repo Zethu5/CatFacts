adfcf04adaa9069e215d09c77ab5a5bb9bbb81ab
Zethus <ravet.zvika93@gmail.com>
2023-04-21 02:32:49 +0300

    here we go

--- catfacts/urls.py
+++ catfacts/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from facts.views import catfact
 
+#test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catfact/', catfact)
