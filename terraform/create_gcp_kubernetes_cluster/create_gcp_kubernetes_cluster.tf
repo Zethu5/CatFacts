e3d2df50c1ea174fba1588c2ad21e2be1ca78e4f
Zethus <ravet.zvika93@gmail.com>
2023-04-20 20:10:59 +0300

    fix

--- terraform/create_gcp_kubernetes_cluster/create_gcp_kubernetes_cluster.tf
+++ terraform/create_gcp_kubernetes_cluster/create_gcp_kubernetes_cluster.tf
@@ -34,7 +34,6 @@ resource "google_container_cluster" "cluster" {
       machine_type = "e2-micro"
       disk_size_gb = 10
       disk_type    = "pd-standard"
-      preemptible  = true
     }
 
     initial_node_count = 1
